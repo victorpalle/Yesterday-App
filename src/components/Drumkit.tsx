7dc7e88f6ffe5080a1fc61ed400d32947772d6fa
Victor PALLE <victor.palle@epitech.eu>
2024-05-25 22:26:55 +0200

    tried toucch end

--- src/components/Drumkit.tsx
+++ src/components/Drumkit.tsx
@@ -5,19 +5,35 @@ import {
   WorkSpacesState,
 } from "../types/WorkSpace";
 import { playAudioWithNewAudioPlayer } from "../tools/playAudio";
+import { useKeyPress } from "../hooks/UseKeyPress";
 
 interface DrumpadProps {
   onClick: (drumpadIndex: number) => void;
-  workspaceState: WorkSpaceBoxState;
+  workSpaceBoxState: WorkSpaceBoxState;
 }
 
-const Drumpad: FC<DrumpadProps> = ({ onClick, workspaceState }) => {
+const Drumpad: FC<DrumpadProps> = ({ onClick, workSpaceBoxState }) => {
+
+
+  const onKeyPressed = (event: any) => {
+    if (workSpaceBoxState.sample) {
+      if (workSpaceBoxState.englishKey === event.key) {
+        const url = workSpaceBoxState.sample.filename
+        // playAudio(url, audioRef);
+        if (url) {
+          playAudioWithNewAudioPlayer(url);
+        }
+      }
+    }
+  };
+  useKeyPress(['a', 'z', 'e', 'r', 'q', 's', 'd', 'f', 'q', 'w'], onKeyPressed);
   return (
     <div
-      onClick={() => onClick(workspaceState.index)}
+      // onClick={() => onClick(workSpaceBoxState.index)}
+      onTouchEnd={() => onClick(workSpaceBoxState.index)}
       className="flex h-full w-[30%] border-solid border-2 rounded-lg items-center justify-center"
     >
-      {workspaceState.sample?.nickName}
+      {workSpaceBoxState.sample?.nickName}
     </div>
   );
 };
@@ -26,25 +42,30 @@ const Drumkit: FC = () => {
   const [workSpacesState, setWorkSpacesState] = useState<WorkSpacesState>(
     KitsInitialState.kits[0]
   );
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const audioContextRef = useRef<AudioContext | null>(null);
   const audioBuffersRef = useRef<{ [key: string]: AudioBuffer }>({});
 
   useEffect(() => {
     if (!audioContextRef.current) {
-      audioContextRef.current = new (window.AudioContext || (window as any).webkitAudioContext)();
+      audioContextRef.current = new (window.AudioContext ||
+        (window as any).webkitAudioContext)();
     }
   }, []);
 
   const loadAudio = async (url: string) => {
     const response = await fetch(url);
     const arrayBuffer = await response.arrayBuffer();
-    const audioBuffer = await audioContextRef.current!.decodeAudioData(arrayBuffer);
+    const audioBuffer = await audioContextRef.current!.decodeAudioData(
+      arrayBuffer
+    );
     audioBuffersRef.current[url] = audioBuffer;
   };
 
   // Preload audio files into audio buffers
   useEffect(() => {
+    setIsLoading(true);
     const preloadAudio = async () => {
       for (const box of workSpacesState.boxes) {
         const url = box.sample?.filename;
@@ -54,48 +75,57 @@ const Drumkit: FC = () => {
       }
     };
     preloadAudio();
+    setIsLoading(false);
   }, [workSpacesState]);
 
-  const onClickOnDrumPad = useCallback((drumpadIndex: number) => {
-    if (!audioContextRef.current) return;
+  const onClickOnDrumPad = useCallback(
+    (drumpadIndex: number) => {
+      if (!audioContextRef.current) return;
 
-    const url = workSpacesState.boxes[drumpadIndex].sample?.filename;
-    if (url && audioBuffersRef.current[url]) {
-      const source = audioContextRef.current.createBufferSource();
-      source.buffer = audioBuffersRef.current[url];
-      source.connect(audioContextRef.current.destination);
-      source.start(0);
-    }
-  }, [workSpacesState]);
+      const url = workSpacesState.boxes[drumpadIndex].sample?.filename;
+      if (url && audioBuffersRef.current[url]) {
+        const source = audioContextRef.current.createBufferSource();
+        source.buffer = audioBuffersRef.current[url];
+        source.connect(audioContextRef.current.destination);
+        source.start(0);
+      }
+    },
+    [workSpacesState]
+  );
 
   const onClickOnKitButton = (index: number) => {
     console.log(index);
     setWorkSpacesState(KitsInitialState.kits[index]);
   };
-  console.log(workSpacesState)
   return (
     <div onClick={() => audioContextRef.current?.resume()}>
-      <div className="flex w-full py-9 h-44 items-center justify-center space-x-4">
-        {workSpacesState.boxes.map((box) => (
-          <Drumpad
-            key={box.index}
-            onClick={onClickOnDrumPad}
-            workspaceState={box}
-          />
-        ))}
-      </div>
-      Kits :
-      <div className="flex w-full py-9 h-44 items-center justify-center space-x-4">
-        {KitsInitialState.kits.map((kit, index) => (
-          <div
-            key={index}
-            onClick={() => onClickOnKitButton(index)}
-            className="flex h-full w-[30%] border-solid border-2 rounded-lg items-center justify-center"
-          >
-            {kit.name}
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <>
+          <div className="flex w-full py-9 h-44 items-center justify-center space-x-4">
+            {workSpacesState.boxes.map((box) => (
+              <Drumpad
+                key={box.index}
+                onClick={onClickOnDrumPad}
+                workSpaceBoxState={box}
+              />
+            ))}
           </div>
-        ))}
-      </div>
+          Kits :
+          <div className="flex w-full py-9 h-44 items-center justify-center space-x-4">
+            {KitsInitialState.kits.map((kit, index) => (
+              <div
+                key={index}
+                onClick={() => onClickOnKitButton(index)}
+                className="flex h-full w-[30%] border-solid border-2 rounded-lg items-center justify-center"
+              >
+                {kit.name}
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
